482785af15c329cd243598e1faa999b1e9809da7
Kamlesh Jaiswal <kamlesh.jswal@gmail.com>
2019-08-13 16:04:29 +0530

    changes for rebuild

--- CCI_Fastlane/ViewController.swift
+++ CCI_Fastlane/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("changes for build")
+        print("changes for rebuild")
     }
 
 
